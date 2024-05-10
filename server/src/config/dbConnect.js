72c66abe1c9ca96fc6759b37caa4503ca367ac4a
Syed Hasnain Mehadi <154125636+hasnain-neosoft@users.noreply.github.com>
2024-05-10 13:05:41 +0530

    remove db secret

--- server/src/config/dbConnect.js
+++ server/src/config/dbConnect.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
   try {
-    const URI =process.env.MONGO_URI ??
-      "mongodb+srv://devUser:pfHAOkZ3EAx6zFwB@freecluster.fzhgwor.mongodb.net/booking?retryWrites=true&w=majority&appName=freeCluster";
-    const con = await mongoose.connect(URI);
+    const con = await mongoose.connect(process.env.MONGO_URI);
     console.log("database connected successful");
   } catch (error) {
     console.log("error?.message", error?.message);
